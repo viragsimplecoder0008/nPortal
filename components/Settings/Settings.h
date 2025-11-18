e96abfad63676450e83b3b94b74c18184cb61d04
Satyamedh Hulyalkar <satyamedh9@gmail.com>
2025-11-18 21:28:34 +0530

    Added compile-time settings

---
+++ components/Settings/Settings.h
@@ -0,0 +1,15 @@
+//
+// Created by Satyamedh on 18-11-2025.
+//
+
+#ifndef NPORTAL_SETTINGS_H
+#define NPORTAL_SETTINGS_H
+
+// TODO: for now, this is compiler-configured settings. Will switch to file-based later.
+
+#include "../../nGL/gl.h"
+
+#define MAX_MOVEMENT_SPEED GLFix(5.0f) // units per second
+#define TOUCHPAD_SENSITIVITY 1.0f   // multiplier for touchpad input
+
+#endif //NPORTAL_SETTINGS_H
